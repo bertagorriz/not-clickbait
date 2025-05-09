e945411222dc4aa9017f0169d52dfa954b3716e5
Berta Górriz <bertagorrizfernandez@gmail.com>
2025-05-09 16:40:23 +0200

    feat(features): add features component with horizontal scroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 import { getScrollSmoother } from "./utils/getScrollSmoother.js";
+import { getDynamicTokens } from "./utils/getDynamicTokens.js";
 
 import "../components/intro/intro.js";
 import "../components/hero/hero.js";
 import "../components/definition/definition.js";
 import "../components/history/history.js";
 import "../components/counter/counter.js";
+import "../components/features/features.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   getScrollSmoother();
+  getDynamicTokens();
   console.log("Content initialized");
 });
 
