9670750656c04e0a538a0b923240ec45b68365af
Berta Górriz <bertagorrizfernandez@gmail.com>
2025-05-09 11:24:44 +0200

    feat(history): add animated history component and `debounce` function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { getScrollSmoother } from "./utils/getScrollSmoother.js";
 import "../components/intro/intro.js";
 import "../components/hero/hero.js";
 import "../components/definition/definition.js";
+import "../components/history/history.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   getScrollSmoother();
